32435abf83f1a614af4169765e26e2dc6f3b3ea9
Nikita Ahuja <nikita.ahuja@dignityhealthcd.onmicrosoft.com>
2023-10-18 11:29:10 -0400

    update

--- c1-versions.tf
+++ c1-versions.tf
@@ -15,7 +15,7 @@ terraform {
     resource_group_name = "terraform-storage-rg"
     storage_account_name = "terraformstate1"
     container_name = "tfstatefiles"
-    key = "tf-test1.tfstate"
+    key = "tf-test2.tfstate"
   }
 }
 #this is 
