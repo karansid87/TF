82e7d956811a97f1023f01be0b6ec219b4b2e0d2
Nikita Ahuja <nikita.ahuja@dignityhealthcd.onmicrosoft.com>
2023-10-24 18:18:22 -0400

    update

--- New/c4-virtual-network.tf
+++ New/c4-virtual-network.tf
@@ -44,8 +44,8 @@ resource "azurerm_public_ip" "mypublicip" {
   resource_group_name = azurerm_resource_group.myrg.name
   location            = azurerm_resource_group.myrg.location
   allocation_method   = "Static"
-  #domain_name_label = "app1-vm-${random_string.myrandom[each.key].id}"
-  domain_name_label = "app1-vm-${each.key}-${random_string.myrandom.id}"
+  domain_name_label = "app1-vm-${random_string.myrandom[each.key].id}"
+  #domain_name_label = "app1-vm-${each.key}-${random_string.myrandom.id}"
   tags = {
     environment = each.key
   }
