115653239384fa292a5b9a3dcf4edf48a461845f
Nikita Ahuja <nikita.ahuja@dignityhealthcd.onmicrosoft.com>
2023-10-24 18:06:02 -0400

    update

--- New/c2-variables.tf
+++ New/c2-variables.tf
@@ -10,7 +10,8 @@ variable "business_unit" {
 variable "environment" {
   description = "Environment Name"
   type = set(string)
-  default = ["controller", "hub", "db", "master"]
+  #default = ["controller", "hub", "db", "master"]
+  default = ["test"]
 }
 # 3. Resource Group Name
 variable "resoure_group_name" {
