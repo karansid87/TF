f4f9ebf9dea43cd1c25bf5c772488dfc74fa675e
Nikita Ahuja <nikita.ahuja@dignityhealthcd.onmicrosoft.com>
2023-10-30 12:36:29 -0400

    update

--- N3/c4-virtual-network.tf
+++ N3/c4-virtual-network.tf
@@ -33,7 +33,7 @@ resource "azurerm_subnet" "mysubnet" {
   name                 = "mysubnet-1"
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.3.0/24"]
 }
 
 # Create Public IP Address
