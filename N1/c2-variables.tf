fd077890b6953bec5816a47dcfcd20256e9f6b1f
Nikita Ahuja <nikita.ahuja@dignityhealthcd.onmicrosoft.com>
2023-10-29 20:54:23 -0400

    update

---
+++ N1/c2-variables.tf
@@ -0,0 +1,34 @@
+# Input Variables
+
+# 1. Business Unit Name
+variable "business_unit" {
+  description = "Business Unit Name"
+  type = string
+  default = "hr"
+}
+# 2. Environment Name
+variable "environment" {
+  description = "Environment Name"
+  type = set(string)
+  default = ["controller","hub", "db", "master"]
+  #default = ["test"]
+}
+# 3. Resource Group Name
+variable "resoure_group_name" {
+  description = "Resource Group Name"
+  type = string
+  default = "karan"
+}
+# 4. Resource Group Location
+variable "resoure_group_location" {
+  description = "Resource Group Location"
+  type = string
+  default = "East US"
+}
+
+# 5. Virtual Network Name
+variable "virtual_network_name" {
+  description = "Virtual Network Name"
+  type = string 
+  default = "myvnet1"
+}
