e3445570bf874c1ae1226ad089df46813c0e2d8c
karansid87 <148272816+karansid87@users.noreply.github.com>
2023-11-07 13:01:26 -0500

    Update c2-variables.tf

--- N1/c2-variables.tf
+++ N1/c2-variables.tf
@@ -10,7 +10,7 @@ variable "business_unit" {
 variable "environment" {
   description = "Environment Name"
   type = set(string)
-  default = ["controller"]
+  default = ["controller","hub"]
   #default = ["test"]
 }
 # 3. Resource Group Name
